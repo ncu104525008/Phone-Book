b8dbd9303c48f8ae1ef5eef58a78e4091d9d6232
Sango <g104525008@g.ncu.edu.tw>
2016-04-27 15:01:49 +0800

    finish

--- app/src/main/java/com/example/sango/ghw4/MainActivity.java
+++ app/src/main/java/com/example/sango/ghw4/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.sango.ghw4;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -25,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     private String[] list;
     private ArrayAdapter<String> listAdapter;
 
+    private final static String CALL = "android.intent.action.CALL";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +45,19 @@ public class MainActivity extends AppCompatActivity {
         db = MyDBHelper.getDatabase(MainActivity.this);
 
         listView = (ListView)findViewById(R.id.listView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                callPhone(position);
+            }
+        });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                myListAlertDialog(position);
+                return true;
+            }
+        });
 
         showPhones();
 
@@ -44,6 +66,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, PhoneActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putInt("phoneId", -1);
+                intent.putExtras(bundle);
                 startActivity(intent);
                 finish();
             }
@@ -101,5 +126,74 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return cardList;
+    }
+
+    private void callPhone(int id) {
+        PhoneCard phone = phones.get(id);
+        String phoneNumber = phone.getPhone();
+        Log.w("callPhone: ", phoneNumber);
+        Intent call = new Intent(CALL, Uri.parse("tel:" + phoneNumber));
+        startActivity(call);
+    }
+
+    private void myListAlertDialog(final int id) {
+        final String[] ListStr = { "撥打電話", "修改電話", "刪除電話"};
+        AlertDialog.Builder MyListAlertDialog = new AlertDialog.Builder(this);
+        MyListAlertDialog.setTitle("選擇動作");
+        // 建立List的事件
+        DialogInterface.OnClickListener ListClick = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        callPhone(which);
+                        break;
+                    case 1:
+                        PhoneCard card = getPhoneCard(phones.get(id).getId());
+                        Intent intent = new Intent(MainActivity.this, PhoneActivity.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putInt("phoneId", card.getId());
+                        intent.putExtras(bundle);
+                        startActivity(intent);
+                        break;
+                    case 2:
+                        delPhoneCard(phones.get(id).getId());
+                        break;
+                }
+            }
+        };
+        // 建立按下取消什麼事情都不做的事件
+        DialogInterface.OnClickListener OkClick = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        };
+        MyListAlertDialog.setItems(ListStr, ListClick);
+        MyListAlertDialog.setNeutralButton("取消", OkClick);
+        MyListAlertDialog.show();
+    }
+
+    private PhoneCard getPhoneCard(int id) {
+        Log.w("getPhoneCard: id = ",  ""+id);
+        String where = MyDBHelper.KEY_ID + "=" + id;
+        Cursor cursor = db.query(MyDBHelper.TABLE_NAME, null, where, null, null, null, null, null);
+        PhoneCard card = null;
+
+        while (cursor.moveToNext()) {
+            int pid = cursor.getInt(0);
+            String name = cursor.getString(1);
+            String phone = cursor.getString(2);
+            int status = cursor.getInt(3);
+
+            card = new PhoneCard(pid, name, phone, status);
+        }
+        return card;
+    }
+
+    private void delPhoneCard(int id) {
+        ContentValues cv = new ContentValues();
+
+        cv.put(MyDBHelper.STATUS_COLUMN, -1);
+        String where = MyDBHelper.KEY_ID + "=" + id;
+        db.update(MyDBHelper.TABLE_NAME, cv, where, null);
+        showPhones();
     }
 }
--- app/src/main/java/com/example/sango/ghw4/PhoneActivity.java
+++ app/src/main/java/com/example/sango/ghw4/PhoneActivity.java
@@ -2,12 +2,14 @@ package com.example.sango.ghw4;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,6 +23,7 @@ public class PhoneActivity extends AppCompatActivity {
     private EditText nameText;
     private EditText phoneText;
     private SQLiteDatabase db;
+    private int phoneId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,10 @@ public class PhoneActivity extends AppCompatActivity {
         setContentView(R.layout.activity_phone);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Bundle bundle = this.getIntent().getExtras();
+        phoneId = bundle.getInt("phoneId");
+        Log.w("onCreate: phoneId=", ""+phoneId);
 
         db = MyDBHelper.getDatabase(PhoneActivity.this);
 
@@ -41,14 +48,23 @@ public class PhoneActivity extends AppCompatActivity {
                 cv.put(MyDBHelper.NAME_COLUMN, nameText.getText().toString());
                 cv.put(MyDBHelper.PHONE_COLUMN, phoneText.getText().toString());
                 cv.put(MyDBHelper.STATUS_COLUMN, 0);
+                String where = MyDBHelper.KEY_ID + "=" + phoneId;
 
-                db.insert(MyDBHelper.TABLE_NAME, null, cv);
+                if(phoneId == -1) {
+                    db.insert(MyDBHelper.TABLE_NAME, null, cv);
+                } else {
+                    db.update(MyDBHelper.TABLE_NAME, cv, where, null);
+                }
 
                 Intent intent = new Intent(PhoneActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
+
+        if(phoneId != -1) {
+            setTextView();
+        }
     }
 
     @Override
@@ -71,5 +87,27 @@ public class PhoneActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private PhoneCard getPhoneCard(int id) {
+        String where = MyDBHelper.KEY_ID + "=" + id;
+        Cursor cursor = db.query(MyDBHelper.TABLE_NAME, null, where, null, null, null, null, null);
+        PhoneCard card = null;
+
+        while (cursor.moveToNext()) {
+            int pid = cursor.getInt(0);
+            String name = cursor.getString(1);
+            String phone = cursor.getString(2);
+            int status = cursor.getInt(3);
+
+            card = new PhoneCard(id, name, phone, status);
+        }
+        return card;
+    }
+
+    private void setTextView() {
+        PhoneCard card = getPhoneCard(phoneId);
+        nameText.setText(card.getName());
+        phoneText.setText(card.getPhone());
     }
 }
